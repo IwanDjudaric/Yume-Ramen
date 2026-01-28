a289f027c37504fce1a82a13a243feedb28e47f5
Zero <102381@glr.nl>
2026-01-28 08:08:13 +0000

    add config

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$servername =  'localhost';
+$username =   'Yume_ramen_102381';
+$password =   'Yume_ramen_102381!';
+$dbname = '102381_CRUD';
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
+
+try {
+    $PDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $options);
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
